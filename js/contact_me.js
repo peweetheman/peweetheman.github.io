59e1c23156300734d3bc7e3128a50c820d2ecd3e
Patrick Phillips <pphill10@u.rochester.edu>
2020-04-17 18:29:38 -0400

    My First Commit

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,11 +33,11 @@ $(function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
+              .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+              .append("<strong>Your message has been sent. </strong>");
           $('#success > .alert-success')
-            .append('</div>');
+              .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
@@ -45,7 +45,7 @@ $(function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
+              .append("</button>");
           $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
